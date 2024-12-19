498c9152f2003c7c26cb06193c93c5676de65807
Samuil Borisov <samuil.bo5@gmail.com>
2024-12-19 11:06:48 +0200

    update ERC20

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,53 +1,73 @@
 pragma solidity >=0.4.23;
 
 contract MyToken {
-    string public name;
-    string public symbol;
-    uint8 public decimals = 18;
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals = 18;
+    uint256 private _totalSupply;
 
-    constructor(string _name, string _symbol) public {
-        name = _name;
-        symbol = _symbol;
-        uint256 initialSupply = 1000000000 * 10 ** uint(decimals);
-        balanceOf[msg.sender] = initialSupply;
-        totalSupply_ = initialSupply;
+    constructor(string name, string symbol) public {
+        _name = name;
+        _symbol = symbol;
+        uint256 initialSupply = 1000000000 * 10 ** uint256(_decimals);
+        _balances[msg.sender] = initialSupply;
+        _totalSupply = initialSupply;
     }
 
-    event Approval(address indexed src, address indexed guy, uint wad);
-    event Transfer(address indexed src, address indexed dst, uint wad);
+    event Approval(address indexed src, address indexed guy, uint256 wad);
+    event Transfer(address indexed src, address indexed dst, uint256 wad);
 
-    uint256 private totalSupply_;
-    mapping(address => uint) public balanceOf;
-    mapping(address => mapping(address => uint)) public allowance;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) public _allowances;
 
-    function totalSupply() public view returns (uint) {
-        return totalSupply_;
+    function name() public view returns (string) {
+        return _name;
     }
 
-    function approve(address guy, uint wad) public returns (bool) {
-        allowance[msg.sender][guy] = wad;
+    function symbol() public view returns (string) {
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address guy) public view returns (uint256) {
+        return _balances[guy];
+    }
+
+    function allowance(address src, address guy) public view returns (uint256) {
+        return _allowances[src][guy];
+    }
+
+    function approve(address guy, uint256 wad) public returns (bool) {
+        _allowances[msg.sender][guy] = wad;
         emit Approval(msg.sender, guy, wad);
         return true;
     }
 
-    function transfer(address dst, uint wad) public returns (bool) {
+    function transfer(address dst, uint256 wad) public returns (bool) {
         return transferFrom(msg.sender, dst, wad);
     }
 
     function transferFrom(
         address src,
         address dst,
-        uint wad
+        uint256 wad
     ) public returns (bool) {
-        require(balanceOf[src] >= wad);
+        require(_balances[src] >= wad);
 
-        if (src != msg.sender && allowance[src][msg.sender] != uint(-1)) {
-            require(allowance[src][msg.sender] >= wad);
-            allowance[src][msg.sender] -= wad;
+        if (src != msg.sender && _allowances[src][msg.sender] != uint256(-1)) {
+            require(_allowances[src][msg.sender] >= wad);
+            _allowances[src][msg.sender] -= wad;
         }
 
-        balanceOf[src] -= wad;
-        balanceOf[dst] += wad;
+        _balances[src] -= wad;
+        _balances[dst] += wad;
 
         emit Transfer(src, dst, wad);
 
